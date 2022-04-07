f8c07bc7295ba5d19e532334162c8c6e488afe74
snehi <snehil101@gmail.com>
2022-04-07 12:52:58 +0530

    Initial project

---
+++ app/src/main/java/com/otaku/fetch/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.otaku.fetch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
