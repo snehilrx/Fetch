8a3a0964bb2f72b052dcb36ae337f991643ba5d2
snehi <snehil101@gmail.com>
2022-04-11 21:57:12 +0530

    hilt changes

---
+++ app/src/main/java/com/otaku/fetch/FetchApplication.kt
@@ -0,0 +1,7 @@
+package com.otaku.fetch
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FetchApplication : Application()
---
+++ base/src/main/java/com/otaku/base/di/modules/FragmentModule.kt
@@ -0,0 +1,9 @@
+package com.otaku.base.di.modules
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.FragmentComponent
+
+@Module
+@InstallIn(FragmentComponent::class)
+object FragmentModule
