ac4705fbd4921f04c147ae377b5cf73734b80d9f
snehi <snehil101@gmail.com>
2022-04-11 21:57:12 +0530

    hilt changes

---
+++ base/src/main/java/com/otaku/base/di/modules/ActivityModule.kt
@@ -0,0 +1,9 @@
+package com.testing.skeletonapp.di.modules
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+object ActivityModule
---
+++ base/src/main/java/com/otaku/base/di/modules/ServiceModule.kt
@@ -0,0 +1,9 @@
+package com.otaku.base.di.modules
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule
