530159e9ee22c99fa1f6b2b3509fc050e75f26b8
snehi <snehil101@gmail.com>
2022-04-12 09:55:03 +0530

    added kickassanime Api
    Signed-off-by: snehi <snehil101@gmail.com>

--- kickassanime/src/main/java/com/otaku/kickassanime/api/model/AnimeListFrontPageResponse.kt
+++ kickassanime/src/main/java/com/otaku/kickassanime/api/model/AnimeListFrontPageResponse.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class Anime (
     @SerializedName("episode"      ) val episode     : String? = null,
+    /** anime/boruto-naruto-next-generations-923495/episode-245-958365 */
     @SerializedName("slug"         ) val slug        : String? = null,
     @SerializedName("type"         ) val type        : String? = null,
     @SerializedName("episode_date" ) val episodeDate : String? = null,
---
+++ kickassanime/src/main/java/com/otaku/kickassanime/api/model/EpisodeInformation.kt
@@ -0,0 +1,24 @@
+package com.otaku.kickassanime.api.model
+
+import com.google.gson.annotations.SerializedName
+
+data class EpisodeInformation(
+    @SerializedName("name") val name: String? = null,
+    @SerializedName("title") val title: String? = null,
+    @SerializedName("slug") val slug: String? = null,
+    @SerializedName("slug_id") val slugId: String? = null,
+    @SerializedName("dub") val dub: String? = null,
+    @SerializedName("link1") val link1: String? = null,
+    @SerializedName("link2") val link2: String? = null,
+    @SerializedName("link3") val link3: String? = null,
+    @SerializedName("link4") val link4: String? = null,
+    @SerializedName("anime_id") val animeId: String? = null,
+    @SerializedName("sector") val sector: String? = null,
+    @SerializedName("createddate") val createdDate: String? = null,
+    @SerializedName("next") val next: PlaylistTrack? = null,
+    @SerializedName("prev") val prev: PlaylistTrack? = null,
+    @SerializedName("epid") val epId: String? = null,
+    @SerializedName("rating") val rating: Int? = null,
+    @SerializedName("votes") val votes: String? = null,
+    @SerializedName("favorited") val favourite: Boolean? = null
+)
