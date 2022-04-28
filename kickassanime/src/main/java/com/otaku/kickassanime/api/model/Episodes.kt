44afa9afc8eb0bd831e15a03601749160517a82d
snehi <snehil101@gmail.com>
2022-04-28 15:18:18 +0530

    kickassanime api changes Signed-off-by: snehi <snehil101@gmail.com>
    Signed-off-by: snehi <snehil101@gmail.com>

--- kickassanime/src/main/java/com/otaku/kickassanime/api/model/Episodes.kt
+++ kickassanime/src/main/java/com/otaku/kickassanime/api/model/Episodes.kt
@@ -6,6 +6,7 @@ data class Episodes (
     @SerializedName("epnum") val epnum: String? = null,
     @SerializedName("name") val name: String? = null,
     @SerializedName("slug") val slug: String? = null,
+    // \/anime\/estab-life-great-escape-836031\/episode-06-994213
     @SerializedName("createddate") val createddate: String? = null,
     @SerializedName("num") val num: String? = null
 )
